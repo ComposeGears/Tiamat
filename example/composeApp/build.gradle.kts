cb6e6ed5dae8a9888bacc4f5f5d3689942458d80
Yahor Urbanovich <egorikftp@gmail.com>
2024-02-08 18:13:05 +0300

    Fix iOS compilation

--- example/composeApp/build.gradle.kts
+++ example/composeApp/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeGears-Nav-Example"
+            baseName = "ComposeApp"
             isStatic = true
         }
     }
