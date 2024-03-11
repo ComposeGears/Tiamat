76f00b4b4dfea084b61be17cdb9208ae1cad904a
Yahor Urbanovich <egorikftp@gmail.com>
2024-03-11 10:09:29 +0300

    remove koin-bom

--- tiamat-koin/build.gradle.kts
+++ tiamat-koin/build.gradle.kts
@@ -30,7 +30,6 @@ kotlin {
         commonMain.dependencies {
             implementation(projects.tiamat)
 
-            implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
 
