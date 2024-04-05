6cd37b523a51635b32b3b272f4aba0a4f921e2f8
egorikftp <egorikftp@gmail.com>
2024-04-05 17:20:58 +0300

    Add Wasm overlay

--- example/composeApp/src/wasmJsMain/kotlin/main.kt
+++ example/composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,4 +1,13 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.CanvasBasedWindow
 import content.examples.koin.KoinLib
 
@@ -6,6 +15,15 @@ import content.examples.koin.KoinLib
 fun main() {
     KoinLib.start()
     CanvasBasedWindow(canvasElementId = "ComposeTarget") {
-        App()
+        Box {
+            App()
+            Text(
+                modifier = Modifier.align(Alignment.BottomEnd).padding(32.dp),
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                text = "Pre Alpha (WASM)",
+            )
+        }
     }
 }
