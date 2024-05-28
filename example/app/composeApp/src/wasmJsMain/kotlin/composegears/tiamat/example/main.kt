eefb4ad32c89809f4106e9b251f23ad46d9b3941
egorikftp <egorikftp@gmail.com>
2024-05-28 14:44:32 +0300

    Remove PageLoadNotify

--- example/app/composeApp/src/wasmJsMain/kotlin/composegears/tiamat/example/main.kt
+++ example/app/composeApp/src/wasmJsMain/kotlin/composegears/tiamat/example/main.kt
@@ -5,6 +5,7 @@ package composegears.tiamat.example
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -14,10 +15,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.CanvasBasedWindow
 
+external fun onLoadFinished()
+
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow(canvasElementId = "TiamatTarget") {
-        PageLoadNotify()
+        LaunchedEffect(Unit) {
+            onLoadFinished()
+        }
         Box {
             App()
             Text(
