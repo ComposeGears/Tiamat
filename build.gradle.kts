04f341a12fd4441d670c897244b26bae97be707f
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-08-01 14:51:00 +0300

    Add binary validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.binary.compatibility)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.m2p) apply false
+}
+
+apiValidation {
+    ignoredProjects += listOf("composeApp", "sample-koin", "ui-core")
 }
 
 detekt {
