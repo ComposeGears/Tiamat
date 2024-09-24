8ab3a805a5cc0a6c92d10df00e4dc2dc72fb65c7
Viachaslau Katsuba <vkatsubo@gmail.com>
2024-09-24 22:10:32 +0300

    minor fix

--- example/app/composeApp/build.gradle.kts
+++ example/app/composeApp/build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
