dbeef8b26b14ee4e2f2f093ad4063f02efb68440
Viachaslau Katsuba <vkatsubo@gmail.com>
2024-11-04 17:46:37 +0300

    add doc

--- tiamat/src/commonMain/kotlin/com/composegears/tiamat/Extension.kt
+++ tiamat/src/commonMain/kotlin/com/composegears/tiamat/Extension.kt
@@ -2,6 +2,9 @@ package com.composegears.tiamat
 
 import androidx.compose.runtime.Composable
 
+/**
+ * Extension base class
+ */
 public abstract class Extension<Args> {
 
     @Composable
@@ -9,9 +12,17 @@ public abstract class Extension<Args> {
         scope.content()
     }
 
+    /**
+     * The content of extension
+     */
     @Composable
     public abstract fun NavDestinationScope<Args>.content()
 }
 
+/**
+ * Provides an attached extension of defined type
+ *
+ * @return extension or null if the ext of this type is not attached
+ */
 public inline fun <reified P : Extension<*>> NavDestination<*>.ext(): P? =
     extensions.firstOrNull { it is P } as? P?
