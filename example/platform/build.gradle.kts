cae2a9ce08060825802a27ee26a79e1f1e0af36d
Yap <egorikftp@gmail.com>
2025-03-19 02:25:38 +0300

    Migrate CameraX platform sample to new architecture (#139)

--- example/platform/build.gradle.kts
+++ example/platform/build.gradle.kts
@@ -37,6 +37,10 @@ kotlin {
         }
         androidMain.dependencies {
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.camera.camera2)
+            implementation(libs.androidx.camera.lifecycle)
+            implementation(libs.androidx.camera.view)
+            implementation(libs.androidx.concurrent.futures)
         }
     }
 }
