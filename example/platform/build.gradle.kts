f4e190c0f7090e74b6c186ea120c6d504f4d1f17
Yahor <egorikftp@gmail.com>
2025-08-18 11:16:21 +0300

    Remove unnecessary dependency on androidx.concurrent.futures and simplify camera provider usage

--- example/platform/build.gradle.kts
+++ example/platform/build.gradle.kts
@@ -41,7 +41,6 @@ kotlin {
             implementation(libs.androidx.camera.camera2)
             implementation(libs.androidx.camera.lifecycle)
             implementation(libs.androidx.camera.view)
-            implementation(libs.androidx.concurrent.futures)
         }
     }
 }
